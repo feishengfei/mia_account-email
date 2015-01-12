e07c668ae364cecd3004f8d150572415aaa490c5
Frank Zhou <feishengfei@126.com>
2015-01-12 22:33:48 +0800

    add AccountEmailServiceTest

---
+++ src/test/java/com/liteon/mvnbook/account/AccountEmailServiceTest.java
@@ -0,0 +1,54 @@
+package com.liteon.mvnbook.account;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.mail.Message;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.icegreen.greenmail.util.GreenMail;
+import com.icegreen.greenmail.util.GreenMailUtil;
+import com.icegreen.greenmail.util.ServerSetup;
+
+public class AccountEmailServiceTest 
+{
+	private GreenMail greenMail;
+	
+	@Before
+	public void startMailServer()
+	{
+		greenMail = new GreenMail(ServerSetup.SMTP);
+		greenMail.setUser("feishengfei@126.com", "123456");
+		greenMail.start();
+	}
+	
+	@Test
+	public void testSendMail()
+		throws Exception
+	{
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("accout-email.xml");
+		AccountEmailService accountEmailService = (AccountEmailService)ctx.getBean("accountEmailService");
+		
+		String subject = "Test Subject";
+		String htmlText = "<h3>Test</h3>";
+		accountEmailService.sendMail("feishengfei@126.com", "test2feishengfei.com", htmlText);
+			
+		greenMail.waitForIncomingEmail(2000, 1);
+		
+		Message[] msgs = greenMail.getReceivedMessages();
+		assertEquals(1, msgs.length);
+		assertEquals(subject, msgs[0].getSubject());
+		assertEquals(htmlText, GreenMailUtil.getBody(msgs[0]).trim());
+	}
+	
+	@After
+	public void stopMailServer()
+		throws Exception
+	{
+		greenMail.stop();
+	}
+}
